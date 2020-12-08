e89c7ce1e2e4774ff7f418433c33088a8e68a5cb
Michał Mordawski <michal.mordawski@amartus.com>
2020-12-08 18:42:53 +0100

    KeyValue plugin boilerplate

--- etcd_plugin/tests/kv/test_kv.py
+++ etcd_plugin/tests/kv/test_kv.py
@@ -1,5 +1,44 @@
 import unittest
 
+from etcd_plugin.resources.kv.kv import KeyValue
+
 
 class KeyValueTestCase(unittest.TestCase):
-    pass
+    def test_create(self):
+        # arrange
+        kv = KeyValue()
+        # mock put
+
+        # act
+        kv.create('foo', 'bar')
+
+        # assert
+        pass
+
+    def test_create_ephemeral(self):
+        # arrange
+        kv = KeyValue()
+        # mock put
+        lease = ...
+
+        # act
+        kv.create('foo', 'bar', lease=lease)
+
+        # assert
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_delete_prefix(self):
+        pass
+
+    def test_get(self):
+        pass
+
+    def test_get_prefix(self):
+        pass
+
+    def test_transaction(self):
+        pass
+
