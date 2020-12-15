de1c2d191d7bc7d92f63a50cd130b529db9d5d9c
Michał Mordawski <michal.mordawski@amartus.com>
2020-12-15 19:16:27 +0100

    SDK KeyValue tests

---
+++ etcd_sdk/tests/base.py
@@ -0,0 +1,44 @@
+# Standard imports
+import unittest
+import mock
+
+# Third party imports
+import etcd3.exceptions as exceptions
+
+
+class EtcdSDKTestBase(unittest.TestCase):
+
+    def setUp(self):
+        super(EtcdSDKTestBase, self).setUp()
+        self.connection = mock.patch('etcd3.client', mock.MagicMock())
+
+    def tearDown(self):
+        super(EtcdSDKTestBase, self).tearDown()
+
+    def get_etcd_connections(self):
+        return {
+            'keyvaluepair': self._fake_keyvaluepair,
+        }
+
+    @property
+    def client_config(self):
+        return {}
+
+    def _gen_etcd_sdk_error(self, message='SomeThingIsGoingWrong'):
+        return mock.MagicMock(
+            side_effect=exceptions.PreconditionFailedError())
+
+    def generate_fake_etcd_connection(self, service_type):
+        return self.get_etcd_connections()[service_type]()
+
+    def _fake_keyvaluepair(self):
+        self.connection.put = self._gen_etcd_sdk_error()
+        self.connection.get_all = self._gen_etcd_sdk_error()
+        self.connection.get = self._gen_etcd_sdk_error()
+        self.connection.get_prefix = self._gen_etcd_sdk_error()
+        self.connection.get_range = self._gen_etcd_sdk_error()
+        self.connection.lease = self._gen_etcd_sdk_error()
+        self.connection.revoke_lease = self._gen_etcd_sdk_error()
+        self.connection.delete = self._gen_etcd_sdk_error()
+        self.connection.delete_prefix = self._gen_etcd_sdk_error()
+        return self.connection
