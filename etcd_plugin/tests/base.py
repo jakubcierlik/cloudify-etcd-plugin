6bdde7b00e3bffcec9dac418ce004a3795a8c7af
Michał Mordawski <michal.mordawski@amartus.com>
2020-12-21 15:43:44 +0100

    WIP: plugin tests

---
+++ etcd_plugin/tests/base.py
@@ -0,0 +1,133 @@
+# Standard imports
+import copy
+import unittest
+
+# Third party imports
+from cloudify.manager import DirtyTrackingDict
+from cloudify.state import current_ctx
+from cloudify.mocks import (
+    MockCloudifyContext,
+    MockNodeContext,
+)
+
+
+class CustomMockCloudifyContext(MockCloudifyContext):
+    def __init__(self, *args, **kwargs):
+        super(CustomMockCloudifyContext, self).__init__(*args, **kwargs)
+
+    @property
+    def workflow_id(self):
+        return 'workflow'
+
+
+class CustomMockNodeContext(MockNodeContext):
+    def __init__(self,
+                 id=None,
+                 properties=None,
+                 type=None,
+                 type_hierarchy=['cloudify.nodes.Root']):
+        super(CustomMockNodeContext, self).__init__(id=id,
+                                                    properties=properties)
+        self._type = type
+        self._type_hierarchy = type_hierarchy
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def type_hierarchy(self):
+        return self._type_hierarchy
+
+
+class EtcdTestBase(unittest.TestCase):
+    def _to_DirtyTrackingDict(self, origin):
+        if not origin:
+            origin = {}
+        dirty_dict = DirtyTrackingDict()
+        for k in origin:
+            dirty_dict[k] = copy.deepcopy(origin[k])
+        return dirty_dict
+
+    @property
+    def client_config(self):
+        return {
+            'host': 'localhost',
+        }
+
+    @property
+    def resource_config(self):
+        return {
+            'name': 'foo',
+            'description': 'foo'
+        }
+
+    @property
+    def node_properties(self):
+        return {
+            'client_config': self.client_config,
+            'resource_config': self.resource_config
+        }
+
+    @property
+    def runtime_properties(self):
+        return {}
+
+    def get_mock_ctx(self,
+                     test_name,
+                     test_properties={},
+                     test_runtime_properties={},
+                     test_relationships=None,
+                     type_hierarchy=['cloudify.nodes.Root'],
+                     node_type='cloudify.nodes.Root',
+                     test_source=None,
+                     test_target=None,
+                     ctx_operation_name=None):
+
+        operation_ctx = {
+            'retry_number': 0, 'name': 'cloudify.interfaces.lifecycle.'
+        } if not ctx_operation_name else {
+            'retry_number': 0, 'name': ctx_operation_name
+        }
+
+        prop = copy.deepcopy(test_properties or self.node_properties)
+        ctx = CustomMockCloudifyContext(
+            node_id=test_name,
+            node_name=test_name,
+            deployment_id=test_name,
+            properties=prop,
+            runtime_properties=self._to_DirtyTrackingDict(
+                test_runtime_properties or self.runtime_properties
+            ),
+            source=test_source,
+            target=test_target,
+            relationships=test_relationships,
+            operation=operation_ctx
+        )
+
+        ctx._node = CustomMockNodeContext(test_name, prop)
+        # In order to set type for the node, we need to set it using _node
+        # instance
+        ctx._node._type = node_type
+        ctx._node._type_hierarchy = type_hierarchy
+        return ctx
+
+    def _prepare_context_for_operation(self,
+                                       test_name,
+                                       test_properties={},
+                                       test_runtime_properties={},
+                                       test_relationships=None,
+                                       type_hierarchy=['cloudify.nodes.Root'],
+                                       test_source=None,
+                                       test_target=None,
+                                       ctx_operation_name=None):
+        self._ctx = self.get_mock_ctx(
+            test_name=test_name,
+            test_properties=test_properties,
+            test_runtime_properties=test_runtime_properties,
+            test_relationships=test_relationships,
+            type_hierarchy=type_hierarchy,
+            test_source=test_source,
+            test_target=test_target,
+            ctx_operation_name=ctx_operation_name)
+        current_ctx.set(self._ctx)
--- etcd_plugin/tests/kv/test_kv.py
+++ etcd_plugin/tests/kv/test_kv.py
@@ -1,17 +1,38 @@
-import unittest
+# Third party imports
+import mock
+
+# Local imports
+from etcd_plugin.tests.base import EtcdTestBase
+from etcd_plugin import keyvaluepair
 
 
-class KeyValueTestCase(unittest.TestCase):
-    def test_create(self):
+@mock.patch('etcd3.client')
+class KeyValuePairTestCase(EtcdTestBase):
+
+    def setUp(self):
+        super(KeyValuePairTestCase, self).setUp()
+
+    @property
+    def resource_config(self):
+        return {
+            'name': 'test_keyvaluepair',
+            'key': b'test_key',
+            'value': b'test_value',
+        }
+
+    def test_create(self, mock_connection):
         # arrange
-
-        # mock put
+        self._prepare_context_for_operation(
+            test_name='KeyValuePairTestCase',
+            ctx_operation_name='cloudify.interfaces.lifecycle.create')
+        mock_connection().put = mock.MagicMock()
 
         # act
-        # kv.create('foo', 'bar')
+        keyvaluepair.create(etcd_resource=None)
 
         # assert
-        pass
+        self.assertEqual(self._ctx.instance.runtime_properties['test_key'],
+                         'test_value')
 
     def test_create_ephemeral(self):
         # arrange
@@ -25,8 +46,22 @@ class KeyValueTestCase(unittest.TestCase):
         # assert
         pass
 
-    def test_delete(self):
-        pass
+    def test_delete(self, mock_connection):
+        # arrange
+        self._prepare_context_for_operation(
+            test_name='KeyValuePairTestCase',
+            test_runtime_properties={'test_key': 'test_value'},
+            ctx_operation_name='cloudify.interfaces.lifecycle.delete')
+        mock_connection().delete = mock.MagicMock(return_value=True)
+
+        # act
+        keyvaluepair.delete(etcd_resource=None)
+
+        # assert
+        for attr in ['test_key',
+                     'test_value']:
+            self.assertNotIn(attr,
+                             self._ctx.instance.runtime_properties)
 
     def test_delete_prefix(self):
         pass
